15ae3b20c8f4abade14baa1761fb434c03394196
pallabi-sahoo <pallabisahoo098.com>
2023-10-18 00:39:07 +0530

    model trainer added

--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -10,7 +10,6 @@ class DataIngestionConfig:
     unzip_dir: Path
 
 
-
 @dataclass(frozen=True)
 class DataValidationConfig:
     root_dir: Path
@@ -23,3 +22,14 @@ class DataValidationConfig:
 class DataTransformationConfig:
     root_dir: Path
     data_path: Path
+
+
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    train_data_path: Path
+    test_data_path: Path
+    model_name: str
+    alpha: float
+    l1_ratio: float
+    target_column: str
